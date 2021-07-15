1b84a0d7e26d2599b903ab7d1e30d55cafd1f5bc
lai6964 <55221630+lai6964@users.noreply.github.com>
2021-07-15 18:42:38 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -2,18 +2,8 @@ import cv2
 import numpy as np
 from PIL import Image,ImageFont,ImageDraw
 
-fontsize = 50
-font = ImageFont.truetype("STFANGSO.TTF",fontsize,encoding="utf-8")
+img = cv2.imread("tmp.png",cv2.IMREAD_UNCHANGED)
 
-
-img = cv2.imread("1.jpg")
-w,h,_ = img.shape
-img_draw = Image.new('RGB',(w,h),(255,255,255))
-
-draw = ImageDraw.Draw(img_draw)
-draw.text((40,100),"测试中ing",fill=(255,0,0),font=font)
-img_draw.show()
-img_draw = cv2.cvtColor(np.asarray(img_draw),cv2.COLOR_RGB2BGR)
-img[img_draw<255] = img_draw
-
+cv2.imshow("img",img)
+cv2.waitKey(0)
 print(1)
