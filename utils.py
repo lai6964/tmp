3f71e9ce4578f977d2a550acb1ec7c78784a68d4
lai6964 <55221630+lai6964@users.noreply.github.com>
2021-07-16 19:52:35 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,65 +1,11 @@
 import numpy as np
-import cv2
+import cv2, math
 from PIL import Image, ImageFont, ImageDraw
-import random as Random
-import math
-
-def char_box_rotate(rotate_angle, size_src=img.size, text_img):
-    char_boxes = list()
-    angle = rotate_angle / 180 * math.pi
-    rotate_center_x = (size_src[0]) // 2
-    rotate_center_y = (size_src[1]) // 2
-
-    shift_x = 0
-    shift_y = 0
-
-    for char_obj in text_img.char_obj_list:
-        box = [[char_obj.box[0], char_obj.box[1]],
-               [char_obj.box[2], char_obj.box[1]],
-               [char_obj.box[2], char_obj.box[3]],
-               [char_obj.box[0], char_obj.box[3]]]
-
-        for index, coord in enumerate(box):
-            box[index][0] = (coord[0] - rotate_center_x) * math.cos(angle) + (coord[1] - rotate_center_y) * math.sin(
-                angle) + rotate_center_x + shift_x
-            box[index][1] = -(coord[0] - rotate_center_x) * math.sin(angle) + (coord[1] - rotate_center_y) * math.cos(
-                angle) + rotate_center_y + shift_y
-        char_boxes.append(box)
-
-    return char_boxes
-
-
-def get_fontcolor(bg_img):
-    """
-    get font color by mean
-    :param bg_img:
-    :return:
-    """
-
-    if 1:
-        image = np.asarray(bg_img)
-        lab_image = cv2.cvtColor(image, cv2.COLOR_RGB2Lab)
-
-        bg = lab_image[:, :, 0]
-        l_mean = np.mean(bg)
-
-        new_l = Random.random_int(0, 127 - 80) if l_mean > 127 else Random.random_int(127 + 80, 255)
-        new_a = Random.random_int(0, 255)
-        new_b = Random.random_int(0, 255)
-
-        lab_rgb = np.asarray([[[new_l, new_a, new_b]]], np.uint8)
-        rbg = cv2.cvtColor(lab_rgb, cv2.COLOR_Lab2RGB)
-
-        r = rbg[0, 0, 0]
-        g = rbg[0, 0, 1]
-        b = rbg[0, 0, 2]
-
-        return (r, g, b, 255)
 
 def get_min_box(img):
 
-    # if len(img.shape)==3:
-    #     img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    if len(img.shape)==3:
+        img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
     img = 1 - img / 255.0
 
     cols = np.sum(img, axis=1)
@@ -79,11 +25,11 @@ def get_min_box(img):
     return box
 
 
-def get_text_img_none(
+def get_text_img(
         font_size=40,
         font_type_path="STFANGSO.TTF",
         text="测试ing中",
-        back_color=(255, 255, 255),
+        back_color=(255, 255, 255,0),
         font_color=(255, 0, 0),
         pad_size=0):
     """ 画两倍框，再裁剪 """
@@ -93,7 +39,7 @@ def get_text_img_none(
     img_draw = Image.new('RGBA', (text_len * font_size * 2, font_size * 2), back_color)
     draw = ImageDraw.Draw(img_draw)
     draw.text((int(font_size * 0.1), int(font_size * 0.1)), text, fill=font_color, font=font)
-    img_draw.show()
+    # img_draw.show()
     img_draw.save("tmp.png")
     img_draw = cv2.cvtColor(np.asarray(img_draw), cv2.COLOR_RGB2BGR)
     box = get_min_box(img_draw)
@@ -105,21 +51,40 @@ def get_text_img_none(
     return img_draw, box, cut_img
 
 
-def get_text_img_bg(
-        img,
-        font_type_path="STFANGSO.TTF",
-        text="测试ing中",
-        back_color=(255, 255, 255),
-        font_color=(255, 0, 0),
-        pad_size=0):
+def my_rotate_PIL(img_draw, angle=-30.0):
+    angle = angle%360.0 ####逆时针多少度
+    w, h = img_draw.size
+    angle_cv2 = math.radians(angle)#angle
+    rotn_center = (w / 2.0, h / 2.0)
+    angle_cos = math.cos(angle_cv2)
+    angle_sin = math.sin(angle_cv2)
 
-    w,h,c = img.shape
-    img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    font_size = int(h/2)
+    coor_w1 = abs(+ rotn_center[0] * angle_cos - rotn_center[1] * angle_sin)
+    coor_w2 = abs(+ rotn_center[0] * angle_cos + rotn_center[1] * angle_sin)
+    coor_h1 = abs(+ rotn_center[0] * angle_sin + rotn_center[1] * angle_cos)
+    coor_h2 = abs(+ rotn_center[0] * angle_sin - rotn_center[1] * angle_cos)
+
+    rot = img_draw.rotate(angle, expand=1)
+    w, h = rot.size
+    w = w / 2
+    h = h / 2
+    coor = [0, 1, 2, 3]
+    coor[0] = (int(w - coor_w1), int(h + coor_h1))
+    coor[1] = (int(w + coor_w1), int(h + coor_h2))
+    coor[2] = (int(w + coor_w2), int(h - coor_h1))
+    coor[3] = (int(w - coor_w2), int(h - coor_h2))
+    print(coor)
+    return rot, coor
 
 
-    return img
+if __name__ == '__main__':
+    img_draw = Image.open("tmp.png")
+    rot, coor = my_rotate_PIL(img_draw)
 
-
-
-
+    img1 = cv2.cvtColor(np.asarray(rot), cv2.COLOR_RGBA2RGB)
+    cv2.line(img1,coor[0],coor[1],(0,255,0),3)
+    cv2.line(img1,coor[1],coor[2],(0,255,0),3)
+    cv2.line(img1,coor[2],coor[3],(0,255,0),3)
+    cv2.line(img1,coor[0],coor[3],(0,255,0),3)
+    cv2.imshow("img",img1)
+    cv2.waitKey(0)
